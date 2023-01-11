e2c7c98c57cc2685a72921c7d6e416a7576df793
oasjs <oasjatoba1@gmail.com>
2023-01-11 12:48:37 -0300

    Corrects view lock state

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -16,13 +18,27 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
         title: const Text('Email Verification'),
       ),
       body: Column(children: [
-        const Text('Please verify your email adress:'),
+        const Text(
+            'We\'ve sent your an email verification. Please open it to verify your account.'),
+        const Text(
+            'If you haven\' received a verification email yet, press the button below.'),
         TextButton(
           onPressed: () async {
             final user = FirebaseAuth.instance.currentUser;
             await user?.sendEmailVerification();
           },
           child: const Text('Send email verification'),
+        ),
+        TextButton(
+          onPressed: () async {
+            await FirebaseAuth.instance.signOut();
+            if (!mounted) return;
+            Navigator.of(context).pushNamedAndRemoveUntil(
+              loginRoute,
+              (route) => false,
+            );
+          },
+          child: const Text('Restart'),
         )
       ]),
     );
