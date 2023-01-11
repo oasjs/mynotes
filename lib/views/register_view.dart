1a2ec7c436a8cc9357bb5c034f6142ca88ba68d1
oasjs <oasjatoba1@gmail.com>
2023-01-11 12:47:33 -0300

    Replaces logs for ErrorDialog and sends email verification automatically

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -56,22 +55,32 @@ class _RegisterViewState extends State<RegisterView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final userCredential = await FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                        email: email, password: password);
-                devtools.log(userCredential.toString());
+                await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                final user = FirebaseAuth.instance.currentUser;
+                await user?.sendEmailVerification();
+                if (!mounted) return;
+                Navigator.of(context).pushNamed(
+                  verifyEmailRoute,
+                );
               } on FirebaseAuthException catch (e) {
                 switch (e.code) {
                   case 'email-already-in-use':
-                    devtools.log('Email already in use');
+                    await showErrorDialog(context, 'Email already in use');
                     break;
                   case 'weak-password':
-                    devtools.log('Weak password');
+                    await showErrorDialog(context, 'Weak password');
                     break;
                   case 'invalid-email':
-                    devtools.log('Invalid email');
+                    await showErrorDialog(context, 'Invalid email adress');
                     break;
+                  default:
+                    await showErrorDialog(context, 'Error: ${e.code}');
                 }
+              } catch (e) {
+                await showErrorDialog(context, 'Error: ${e.toString()}');
               }
             },
             child: const Text('Register'),
