7f4d4b169c12d13c292a2f3e46b86b54ce2a54ed
oasjs <oasjatoba1@gmail.com>
2023-01-18 18:30:11 -0300

    Adds notes to NotesService stream upon calling it

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -12,11 +12,18 @@ class NotesService {
   List<DatabaseNote> _notes = [];
 
   static final NotesService _shared = NotesService._sharedInstance();
-  NotesService._sharedInstance();
+  NotesService._sharedInstance() {
+    _notesStreamController = StreamController<List<DatabaseNote>>.broadcast(
+      onListen: () {
+        // Upon the arrival of a new listener, all notes in the database
+        // will be added to the NotesService stream
+        _notesStreamController.sink.add(_notes);
+      },
+    );
+  }
   factory NotesService() => _shared;
 
-  final _notesStreamController =
-      StreamController<List<DatabaseNote>>.broadcast();
+  late final StreamController<List<DatabaseNote>> _notesStreamController;
 
   Stream get allNotes => _notesStreamController.stream;
 
