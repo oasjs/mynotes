16731345aa9a7692f2ee654274be348799913701
oasjs <oasjatoba1@gmail.com>
2023-01-18 19:49:58 -0300

    Creates generic dialog function and specializes other dialogs from that

---
+++ lib/utilities/dialogs/error_dialog.dart
@@ -0,0 +1,18 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:mynotes/utilities/dialogs/generic_dialog.dart';
+
+Future<void> showErrorDialog(
+  BuildContext context,
+  String text,
+) {
+  return showGenericDialog<void>(
+    context: context,
+    title: 'An error ocurred',
+    content: text,
+    optionBuilder: () => {
+      'OK': null,
+    },
+  );
+}
