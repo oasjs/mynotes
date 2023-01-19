c9ade5c48d1e46e1a8d27a60e031c5bbec896023
oasjs <oasjatoba1@gmail.com>
2023-01-18 21:32:36 -0300

    Adds extension for extracting argument from build context

---
+++ lib/utilities/generics/get_arguments.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart' show BuildContext, ModalRoute;
+
+extension GetArgument on BuildContext {
+  T? getArgument<T>() {
+    final modalRoute = ModalRoute.of(this);
+    // If we cannot extract the modal route return null
+    if (modalRoute != null) {
+      final args = modalRoute.settings.arguments;
+      if (args != null && args is T) {
+        return args as T;
+      }
+    }
+    return null;
+  }
+}
